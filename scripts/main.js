3e7c0aa70fad7b03d434e2388dbffb04f0755044
Vitali Pantsialei <vitali.pantsialei@gmail.com>
2015-06-01 10:17:53 +0300

    load templates once

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
-//var weather;
 require(['weather_widget/weather', 'weather_widget/data'], function (weatherModule, dataProvider) {
     var data = new dataProvider();
     weather = new weatherModule(3, data);
     weather.set('result');
-    var buttons = document.getElementsByTagName('input');
 })
--- scripts/weather_widget/weather.js
+++ scripts/weather_widget/weather.js
@@ -3,21 +3,14 @@ define([], function () {
         var page = 1;
         var inputData = new dataProvider.getJson();
         var currId;
+        var fieldTemplate;
+        var buttonsTemplate;
         var set = function (id) {
             currId = id;
             var field = document.getElementById(id);
             field.innerHTML = "";
             var index;
-            var fieldTemplate = document.implementation.createDocument('', 'html', null);
             var resultHTML = "";
-            
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'scripts/weather_widget/content/fieldtemplate.html', false);
-            xhr.onreadystatechange = function () {
-                if (this.readyState !== 4) return;
-                fieldTemplate.documentElement.innerHTML = this.responseText;
-            };
-            xhr.send();
 
             for (index = (page - 1) * perPage; index != (inputData.length > page * perPage ? page * perPage : inputData.length) ; ++index) {
                 var cpTemp = fieldTemplate;
@@ -43,23 +36,22 @@ define([], function () {
                 field.innerHTML += cpTemp.documentElement.innerHTML;
             }
 
-            xhr.open('GET', 'scripts/weather_widget/content/buttonstemplate.html', false);
-            xhr.onreadystatechange = function () {
-                if (this.readyState !== 4) return;
-                fieldTemplate.documentElement.innerHTML = this.responseText;
-            };
-            xhr.send();
-
-            var buttons = fieldTemplate.getElementsByTagName('input');
+            var buttons = buttonsTemplate.getElementsByTagName('input');
             if (page != 1) {
                 buttons[0].setAttribute('style', 'display:block');
                 buttons[0].setAttribute('onclick', 'weather.back()');
+            }
+            else {
+                buttons[0].setAttribute('style', 'display:none');
             }
             if (inputData.length > page * perPage) {
                 buttons[1].setAttribute('style', 'display:block');
                 buttons[1].setAttribute('onclick', 'weather.next()');
             }
-            field.innerHTML += fieldTemplate.documentElement.innerHTML;
+            else {
+                buttons[1].setAttribute('style', 'display:none');
+            }
+            field.innerHTML += buttonsTemplate.documentElement.innerHTML;
         }
 
         this.set = function (id) {
@@ -77,6 +69,23 @@ define([], function () {
             ++page;
             set(currId);
         }
+
+        fieldTemplate = document.implementation.createDocument('', 'html', null);
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'scripts/weather_widget/content/fieldtemplate.html', false);
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            fieldTemplate.documentElement.innerHTML = this.responseText;
+        };
+        xhr.send();
+
+        buttonsTemplate = document.implementation.createDocument('', 'html', null);
+        xhr.open('GET', 'scripts/weather_widget/content/buttonstemplate.html', false);
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            buttonsTemplate.documentElement.innerHTML = this.responseText;
+        };
+        xhr.send();
     };
     return retModule;
 })
