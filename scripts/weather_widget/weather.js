ec0ef638a96e582f39a63ef4d9ea9bdf1f55549e
Vitali Pantsialei <vitali.pantsialei@gmail.com>
2015-06-05 13:53:15 +0300

    add date functions

--- scripts/weather_widget/weather.js
+++ scripts/weather_widget/weather.js
@@ -4,6 +4,7 @@ define([], function () {
         var inputData = dataProvider.getDailyJson();
         var hoursData = dataProvider.getHoursJson();
         var currId;
+        var currState = 'hour';
         var fieldTemplate, buttonsTemplate, tabsTemplate, hoursTemplate;
         var today = new Date();
         var options = {
@@ -13,15 +14,15 @@ define([], function () {
         var refreshData = function () {
             inputData = dataProvider.getDailyJson();
             hoursData = dataProvider.getHoursJson();
-            refresh();
+            refreshDaily();
         }
         var back = function () {
             --page;
-            refresh();
+            refreshDaily();
         }
         var next = function () {
             ++page;
-            refresh();
+            refreshDaily();
         }
         var setHours = function () {
             var field = document.getElementById(currId);
@@ -29,10 +30,14 @@ define([], function () {
             var lis = tab[0].getElementsByTagName('li');
             var tabInner = field.getElementsByClassName('tabInner');
             var divs = tabInner[0].getElementsByTagName('div');
+            var buttons = field.getElementsByTagName('input');
             divs[0].setAttribute('style', 'display:block');
             divs[1].setAttribute('style', 'display:none');
             lis[0].setAttribute('class', 'active');
             lis[1].setAttribute('class', '');
+            buttons[0].setAttribute('style', 'display:none');
+            buttons[1].setAttribute('style', 'display:none');
+            currState = 'hour';
         }
         var setDays = function () {
             var field = document.getElementById(currId);
@@ -44,6 +49,8 @@ define([], function () {
             divs[1].setAttribute('style', 'display:block');
             lis[0].setAttribute('class', '');
             lis[1].setAttribute('class', 'active');
+            currState = 'day';
+            refreshDaily();
         }
 
         var set = function (id) {
@@ -54,7 +61,6 @@ define([], function () {
             var tabInner = field.getElementsByClassName('tabInner');
             var tabDivs = tabInner[0].getElementsByTagName('div');
             var lis = field.getElementsByTagName('li');
-            setHours();
 
             tabDivs[0].innerHTML = "";
             tabDivs[1].innerHTML = "";
@@ -70,9 +76,38 @@ define([], function () {
             buttons[0].onclick = back;
             buttons[1].onclick = next;
             buttons[2].onclick = refreshData;
-            refresh();
+            setHours();
+            refreshHours();
+            refreshDaily();
         }
-        var refresh = function () {
+        
+        var refreshHours = function () {
+            var myfield = document.getElementById(currId);
+            var tabInner = myfield.getElementsByClassName('tabInner');
+            var field = tabInner[0].getElementsByTagName('div')[0];
+            var index = 0, secIndex = 0;
+            var fieldsets = field.getElementsByTagName('fieldset');
+            var currDate = new Date(today);
+            var img = field.getElementsByTagName('img');
+            var centers = field.getElementsByTagName('center');
+            var legend = field.getElementsByTagName('h2');
+            var descr = field.getElementsByTagName('h4');
+            var tds = field.getElementsByTagName('td');
+
+            currDate.setHours(0);
+            currDate.setMinutes(0);
+            currDate.setSeconds(0);
+            currDate.setMilliseconds(0);
+            while (getFormat2(currDate) != hoursData.list[secIndex].dt_txt.substr(0, 10))
+                secIndex++;
+            while (getFormat1(currDate) != hoursData.list[secIndex].dt_txt) {
+                tds[index++].setAttribute('style', 'display:none');
+                currDate.setHours(currDate.getHours() + 3);
+            }
+
+        }
+
+        var refreshDaily = function () {
             var myfield = document.getElementById(currId);
             var tabInner = myfield.getElementsByClassName('tabInner');
             var field = tabInner[0].getElementsByTagName('div')[1];
@@ -106,23 +141,40 @@ define([], function () {
                 centers[secIndex * 6 + 5].innerHTML = inputData.list[index].humidity;
             }
 
-            var buttons = myfield.getElementsByTagName('input');
-            if (page != 1) {
-                buttons[0].setAttribute('style', 'display:block');
-            }
-            else {
-                buttons[0].setAttribute('style', 'display:none');
-            }
-            if (inputData.list.length > page * perPage) {
-                buttons[1].setAttribute('style', 'display:block');
-            }
-            else {
-                if ((inputData.list.length) % perPage != 0) {
-                    for (index = (inputData.list.length) % perPage; index != perPage; index++)
-                        fieldsets[index].setAttribute('style', 'display:none');
+            if (currState == 'day') {
+                var buttons = myfield.getElementsByTagName('input');
+                if (page != 1) {
+                    buttons[0].setAttribute('style', 'display:block');
                 }
-                buttons[1].setAttribute('style', 'display:none');
+                else {
+                    buttons[0].setAttribute('style', 'display:none');
+                }
+                if (inputData.list.length > page * perPage) {
+                    buttons[1].setAttribute('style', 'display:block');
+                }
+                else {
+                    if ((inputData.list.length) % perPage != 0) {
+                        for (index = (inputData.list.length) % perPage; index != perPage; index++)
+                            fieldsets[index].setAttribute('style', 'display:none');
+                    }
+                    buttons[1].setAttribute('style', 'display:none');
+                }
             }
+        }
+
+        var withZero = function (input) {
+            if (input < 10)
+                return '0' + input;
+            else
+                return input;
+        }
+
+        var getFormat1 = function (dateTime) {
+            return dateTime.getFullYear() + '-' + withZero(dateTime.getMonth() + 1) + '-' + withZero(dateTime.getDate()) + ' ' + withZero(dateTime.getHours()) + ':' + withZero(dateTime.getMinutes()) + ':' + withZero(dateTime.getSeconds());
+        }
+
+        var getFormat2 = function (dateTime) {
+            return dateTime.getFullYear() + '-' + withZero(dateTime.getMonth() + 1) + '-' + withZero(dateTime.getDate());
         }
 
         this.set = function (id) {
